aae5315816c5fa8759a8ea2495ca0a9b92e8ccb4
saulOMgit <sauloteromelchor@gmail.com>
2024-05-10 13:40:27 +0200

    Seccion 26 hecha

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,18 +1,65 @@
 
+import { useEffect } from 'react';
+import { useAuthStore, useForm } from '../../hooks';
 import './LoginPage.css';
+import Swal from 'sweetalert2';
+
+const loginFormFields = {
+    loginEmail: '',
+    loginPassword: '',
+}
+const registerFormFields = {
+    registerName:'',
+    registerEmail: '',
+    registerPassword: '',
+    registerPassword2: '',
+
+}
 
 export const LoginPage = () => {
+
+    const {startLogin,startRegister, errorMessage} = useAuthStore();
+
+   
+    
+
+    const {loginEmail,loginPassword,onInputChange:onLoginInputChange} = useForm(loginFormFields);
+    const {registerName,registerEmail,registerPassword,registerPassword2,onInputChange:onRegisterInputChange} = useForm(registerFormFields);
+
+    const loginSubmit = ( event ) =>{
+        event.preventDefault();
+        startLogin({email:loginEmail,password:loginPassword});
+    };
+    const registerSubmit = ( event ) =>{
+        event.preventDefault();
+        if(registerPassword !== registerPassword2){
+            Swal.fire('Error en registro', 'Contraseñas no son iguales', 'error');
+            return;
+        }
+        startRegister({name:registerName,email: registerEmail,password:registerPassword})
+    };
+
+    useEffect(() => {
+        if(errorMessage !== undefined) {
+            Swal.fire('Error en la autenticacion', errorMessage, 'error' );
+        };
+
+    }, [errorMessage]);
+
     return (
         <div className="container login-container">
             <div className="row">
                 <div className="col-md-6 login-form-1">
                     <h3>Ingreso</h3>
-                    <form>
+                    <form onSubmit={loginSubmit}>
                         <div className="form-group mb-2">
                             <input 
                                 type="text"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='loginEmail'
+                                value={loginEmail}
+                                onChange={onLoginInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -20,6 +67,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña"
+                                name='loginPassword'
+                                value={loginPassword}
+                                onChange={onLoginInputChange}
                             />
                         </div>
                         <div className="d-grid gap-2">
@@ -34,12 +84,15 @@ export const LoginPage = () => {
 
                 <div className="col-md-6 login-form-2">
                     <h3>Registro</h3>
-                    <form>
+                    <form onSubmit={registerSubmit}>
                         <div className="form-group mb-2">
                             <input
                                 type="text"
                                 className="form-control"
                                 placeholder="Nombre"
+                                name='registerName'
+                                value={registerName}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -47,6 +100,9 @@ export const LoginPage = () => {
                                 type="email"
                                 className="form-control"
                                 placeholder="Correo"
+                                name='registerEmail'
+                                value={registerEmail}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
                         <div className="form-group mb-2">
@@ -54,6 +110,9 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Contraseña" 
+                                name='registerPassword'
+                                value={registerPassword}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
 
@@ -62,6 +121,10 @@ export const LoginPage = () => {
                                 type="password"
                                 className="form-control"
                                 placeholder="Repita la contraseña" 
+                                
+                                name='registerPassword2'
+                                value={registerPassword2}
+                                onChange={onRegisterInputChange}
                             />
                         </div>
 
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,44 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth";
 import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
 
-    const authStatus = 'authenticated'; //'authenticated' // 'not-authenticated'
-  return (
-    <Routes>
-        {
-            (authStatus === 'not-authenticated')        
-        ? <Route path="/auth/*" element={<LoginPage/>}/>        
-        : <Route path="/*" element={<CalendarPage/>}/>
-        }
+    const {status, checkAuthToken} = useAuthStore();
+    // const authStatus = 'not-authenticated'; //'authenticated' // 'not-authenticated'
 
-        <Route path="/*" element={<Navigate to='/auth/login'/>}/>
+    useEffect(() => {
+      checkAuthToken();
+    }, []);
+    
+    if(status === 'checking'){
+      return (
+        <h3>Cargando...</h3>
+      )
+    }
 
-    </Routes>
+
+
+    return (
+      <Routes>
+          {
+              ( status === 'not-authenticated')  
+                  ? (
+                      <>
+                          <Route path="/auth/*" element={ <LoginPage /> } />
+                          <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+                      </>
+                  )
+                  : (
+                      <>
+                          <Route path="/" element={ <CalendarPage /> } />
+                          <Route path="/*" element={ <Navigate to="/" /> } />
+                      </>
+                  )
+          }
+
+      </Routes>
   )
 }
